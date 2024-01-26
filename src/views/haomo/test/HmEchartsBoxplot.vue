621393a7e660bf769a2b37971c2328ad02f02fdc
胡小根 <hxg@haomo.com>
2024-01-26 09:28:22 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

---
+++ src/views/haomo/test/HmEchartsBoxplot.vue
@@ -0,0 +1,16 @@
+<template>
+  <HmEchartsBoxplot url="https://block-design.cn/HmEchartsBoxplot" />
+</template>
+
+<script>
+import HmEchartsBoxplot from '/@/components/built-in/jeecg/echarts-vue3/HmEchartsBoxplot.vue'
+export default {
+  components: {
+    HmEchartsBoxplot
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/haomo/test/TestHmPdfEmbed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <a-button @click="changePdf">替换pdf</a-button>
+    <h1>File</h1>
+    <hm-pdf-embed :src="source1" />
+
+    <!-- <h1>Base64</h1>
+    <vue-pdf-embed :source="source2" /> -->
+  </div>
+</template>
+
+<script>
+import HmPdfEmbed from '/@/components/built-in/jeecg/HmPdfEmbed.vue'
+
+export default {
+  components: {
+    HmPdfEmbed,
+  },
+  data() {
+    return {
+      source1: 'https://hm-temp.oss-cn-beijing.aliyuncs.com/test1.pdf',
+      // source2: 'data:application/pdf;base64,<BASE64_ENCODED_PDF>',
+    }
+  },
+  methods: {
+    changePdf() {
+      console.log(`替换pdf`);
+      this.source1 = 'https://hm-temp.oss-cn-beijing.aliyuncs.com/test3.pdf';
+    }
+  }
+}
+</script>
