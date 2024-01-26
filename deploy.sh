9badc7dec89b592516d05d02033f4c2e9125db2c
毛安琪 <mac@MacdeMac-Pro-2.local>
2024-01-26 19:10:51 +0800

    fix: 修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
 rm -rf src/views/haomo/test
+# cp src/views/haomo/1750448384092672002/page.vue src/views/dashboard/Analysis/index.vue
 npm run build
 rsync -avz --delete --progress dist/* haomo@192.168.1.7:/var/www/html/dkn
