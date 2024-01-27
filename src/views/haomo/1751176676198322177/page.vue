9d589e0578e11d6ec5bfba5cc52bf54168f1a433
胡小根 <hxg@haomo.com>
2024-01-27 17:33:55 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

---
+++ src/views/haomo/1751176676198322177/page.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page card">
+    <div class="page-wrapper"></div>
+  </div>
+</template>
+
+<script>
+import { h } from "vue";
+
+export default {
+  name: "NewActivity",
+  components: {},
+  data() {
+    let self = this;
+    return {};
+  },
+  watch: {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.page {
+}
+</style>
