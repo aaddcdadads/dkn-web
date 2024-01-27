cc1dd89e17dea4e77de55fee6724899ac70c8260
胡小根 <hxg@haomo.com>
2024-01-27 17:36:18 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/views/haomo/1751176676198322177/page.vue
+++ src/views/haomo/1751176676198322177/page.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="page card">
-    <div class="page-wrapper"></div>
+    <div class="page-wrapper">
+      <div class="ele-wrapper ele-wrapper-e37fdb24-4953-4f7d-af46-b1aa9d4f1737">
+        <hm-bg-card
+          width="100%"
+          height="100%"
+          border-radius=""
+          box-shadow-v-shadow=""
+          box-shadow-blur=""
+          class="ele-e37fdb24-4953-4f7d-af46-b1aa9d4f1737"
+        >
+        </hm-bg-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { h } from "vue";
+import HmBgCard from "/@/components/built-in/layout/HmBgCard.vue";
 
 export default {
   name: "NewActivity",
-  components: {},
+  components: {
+    HmBgCard,
+  },
   data() {
     let self = this;
     return {};
@@ -21,5 +36,9 @@ export default {
 
 <style lang="less" scoped>
 .page {
+}
+
+.ele-wrapper-e37fdb24-4953-4f7d-af46-b1aa9d4f1737 {
+  width: 100%;
 }
 </style>
