90df43bf44cf1a32d8b8bcfff87b7d2a52bc91a8
胡小根 <hxg@haomo.com>
2024-01-28 09:31:38 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/views/haomo/1751176676198322177/page.vue
+++ src/views/haomo/1751176676198322177/page.vue
@@ -212,20 +212,18 @@
                                 {
                                   key: '1',
                                   name: '¥0',
-                                  age: 32,
-                                  address:
-                                    'New York No. 1 Lake Park, New York No. 1 Lake Park',
-                                  sexual: '男',
-                                  school: '加里敦大学',
+                                  age: '',
+                                  address: '',
+                                  sexual: '',
+                                  school: '',
                                 },
                                 {
                                   key: '2',
                                   name: '¥ 29.9',
-                                  age: 42,
-                                  address:
-                                    'London No. 2 Lake Park, London No. 2 Lake Park',
-                                  sexual: '男',
-                                  school: '加里敦大学',
+                                  age: '',
+                                  address: '',
+                                  sexual: '',
+                                  school: '',
                                 },
                               ]"
                               :pagination-hidden="true"
