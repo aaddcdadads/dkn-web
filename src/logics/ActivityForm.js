1733e389b66f47e45ba035640b9e703005f4f4d3
胡小根 <hxg@haomo.com>
2024-01-28 17:19:05 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/logics/ActivityForm.js
+++ src/logics/ActivityForm.js
@@ -11,33 +11,46 @@ let logic = {};
 /**
  * 处理
  */
-const formValidate = logic.formValidate = function() {
-  
-}
+const formValidate = (logic.formValidate = function () {});
 
+/**
+ * 发送添加请求
+ */
+const addRequest = (logic.addRequest = async function () {
+  let res = await self.$postAction(`/api/dkn/activity/adds`, {
+    ...self.$refs.activityAddForm.getFormValues(),
+  });
+  self.addRequestData = res;
+});
+
+/**
+ * 发送编辑请求
+ */
+const editRequest = (logic.editRequest = async function () {
+  let res = await self.$postAction(`/api/dkn/activity/edits`, {
+    ...self.$refs.activityAddForm.getFormValues(),
+  });
+  self.editRequestData = res;
+});
 
 /**
  * 逻辑流 saveOrUpdate 入口函数
  */
-const saveOrUpdate = logic.saveOrUpdate = async (pageVm, eventData) => {
-  console.log(`saveOrUpdate: `, pageVm, eventData)
+const saveOrUpdate = (logic.saveOrUpdate = async (pageVm, eventData) => {
+  console.log(`saveOrUpdate: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.saveOrUpdateData = eventData;
 
-  
   formValidate();
-  if(){
-  
-}else{
-  
-}
-
-}
+  if (self.type === 1) {
+    await addRequest();
+    self.$refs.activityTable.getData();
+  } else {
+    await editRequest();
+    self.$message.success("添加成功");
+  }
+});
 
 /********************** end saveOrUpdate 开始 *********************/
 
-
-export {
-  formValidate,
-  saveOrUpdate,
-}
+export { formValidate, addRequest, editRequest, saveOrUpdate };
