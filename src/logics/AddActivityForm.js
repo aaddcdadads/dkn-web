0b1c67781408e69f1d0a358f54b8b4c91037adb9
胡小根 <hxg@haomo.com>
2024-01-29 16:17:42 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/logics/AddActivityForm.js
+++ src/logics/AddActivityForm.js
@@ -7,6 +7,78 @@ let self = {};
 // logic对象保存当前逻辑组对象，可以通过logic.func或者self.func来调用本逻辑组内的所有逻辑单元
 let logic = {};
 
+/********************** addActivty 开始 *********************/
+/**
+ * 发送添加请求
+ */
+const editRequest = (logic.editRequest = async function () {
+  let res = await self.$postAction(`/api/dkn/activity/edits`, {
+    ...self.item,
+    id: self.id,
+  });
+  self.editRequestData = res;
+});
+
+/**
+ * 逻辑流 addActivty 入口函数
+ */
+const addActivty = (logic.addActivty = async (pageVm, eventData) => {
+  console.log(`addActivty: `, pageVm, eventData);
+  self = Object.assign(pageVm, logic);
+  self.addActivtyData = eventData;
+
+  await self.$refs.activityForm.validate();
+  await self.$refs.activityExtForm.validate();
+  await self.$refs.activityTwoForm.validate();
+
+  let activityProjects = [],
+    activityImgs = [],
+    activityExts = [];
+  activityExts = [{ ...self.$refs.activityExtForm.getFormValues() }];
+  if (self.$refs.activityProjectTable.cData.length > 0) {
+    activityProjects = self.$refs.activityProjectTable.cData.map((x) => {
+      delete x.id, x.createTime, x.createBy, x.updateTime, x.updateBy;
+      return x;
+    });
+  }
+  if (self.$refs.activityImgTableOne.cData.length > 0) {
+    activityProjects = self.$refs.activityImgTableOne.cData.map((x) => {
+      delete x.id, x.createTime, x.createBy, x.updateTime, x.updateBy;
+      x.type = 0;
+      return x;
+    });
+  }
+  if (self.$refs.activityImgTableTwo.cData.length > 0) {
+    let list = self.$refs.activityImgTableTwo.cData.map((x) => {
+      delete x.id, x.createTime, x.createBy, x.updateTime, x.updateBy;
+      x.type = 1;
+      return x;
+    });
+    activityProjects = [...activityProjects, ...list];
+  }
+  self.item = {
+    ...self.$refs.activityForm.getFormValues(),
+    ...self.$refs.activityExtForm.getFormValues(),
+    ...self.$refs.activityTwoForm.getFormValues(),
+    activityExts,
+    activityImgs,
+    activityExts,
+    id: self.id,
+  };
+
+  await editRequest();
+  if (self.editRequestData.success) {
+    self.$message.error(self.editRequestData.message);
+    return;
+  }
+  self.$message.success("操作成功");
+  self.$router.push({
+    path: `/haomo/1750448384092672002/page`,
+  });
+});
+
+/********************** end addActivty 开始 *********************/
+
 /********************** ActivityProject 开始 *********************/
 
 /**
@@ -20,4 +92,4 @@ const ActivityProject = (logic.ActivityProject = async (pageVm, eventData) => {
 
 /********************** end ActivityProject 开始 *********************/
 
-export { ActivityProject };
+export { editRequest, addActivty, ActivityProject };
