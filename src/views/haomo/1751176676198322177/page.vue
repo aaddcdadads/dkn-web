35ebabc538f2b9683dcab19cfb3dd771444de214
胡小根 <hxg@haomo.com>
2024-01-30 15:02:04 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/views/haomo/1751176676198322177/page.vue
+++ src/views/haomo/1751176676198322177/page.vue
@@ -370,7 +370,6 @@
                                   value=""
                                   :toolbar-config="{
                                     toolbarKeys: [
-                                      'headerSelect',
                                       'blockquote',
                                       'bold',
                                       'underline',
@@ -398,6 +397,9 @@
                                       'todo',
                                       'divider',
                                       'codeBlock',
+                                      'insertTable',
+                                      'fullScreen',
+                                      'headerSelect',
                                       {
                                         key: 'group-justify',
                                         title: '对齐',
@@ -425,8 +427,6 @@
                                           'uploadImage',
                                         ],
                                       },
-                                      'insertTable',
-                                      'fullScreen',
                                     ],
                                     excludeKeys: [],
                                   }"
@@ -1442,7 +1442,6 @@ export default {
         value: "",
         toolbarConfig: {
           toolbarKeys: [
-            "headerSelect",
             "blockquote",
             "bold",
             "underline",
@@ -1464,6 +1463,9 @@ export default {
             "todo",
             "divider",
             "codeBlock",
+            "insertTable",
+            "fullScreen",
+            "headerSelect",
             {
               key: "group-justify",
               title: "对齐",
@@ -1488,8 +1490,6 @@ export default {
                 '<svg viewBox="0 0 1024 1024"><path d="M959.877 128l0.123 0.123v767.775l-0.123 0.122H64.102l-0.122-0.122V128.123l0.122-0.123h895.775zM960 64H64C28.795 64 0 92.795 0 128v768c0 35.205 28.795 64 64 64h896c35.205 0 64-28.795 64-64V128c0-35.205-28.795-64-64-64zM832 288.01c0 53.023-42.988 96.01-96.01 96.01s-96.01-42.987-96.01-96.01S682.967 192 735.99 192 832 234.988 832 288.01zM896 832H128V704l224.01-384 256 320h64l224.01-192z"></path></svg>',
               menuKeys: ["insertImage", "uploadImage"],
             },
-            "insertTable",
-            "fullScreen",
           ],
           excludeKeys: [],
         },
