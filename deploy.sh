60eb30600ea7ee660ea6f0b452a3584c07d57488
毛安琪 <mac@MacdeMac-Pro-2.local>
2024-02-01 16:48:25 +0800

    fix: 依赖问题

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 set -x
 rm -rf src/views/haomo/test
 # cp src/views/haomo/1750448384092672002/page.vue src/views/dashboard/Analysis/index.vue
+cp docs/compontents/HmColorKit.vue src/components/built-in/jeecg/HmColorKit.vue
 npm run build
 rsync -avz --delete --progress dist/* haomo@192.168.1.7:/var/www/html/dkn
