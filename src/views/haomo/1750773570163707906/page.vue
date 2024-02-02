4bac39fcc6154f2c3b1a91dbfc2b328ba03dbbf4
胡小根 <hxg@haomo.com>
2024-02-02 17:57:22 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/views/haomo/1750773570163707906/page.vue
+++ src/views/haomo/1750773570163707906/page.vue
@@ -1025,13 +1025,19 @@ export default {
       });
     },
     onBatchDeleteClick() {
+      // 检查是否有选中的行
       if (!this.selectedRows || this.selectedRows.length === 0) {
+        // 如果没有选中的行，显示错误消息并退出函数
         this.$message.error("请选择活动");
         return;
       }
+      // 使用 $nextTick 确保在 DOM 更新后执行
       this.$nextTick(() => {
+        // 设置批量编辑对话框的可见性为true
         this.batchEdit.visible = true;
+        // 设置操作状态为3，这可能是一个标识，表示批量删除
         this.allStatus = 3;
+        // 设置提示文本
         this.prompt.text = "确认批量删除";
       });
     },
