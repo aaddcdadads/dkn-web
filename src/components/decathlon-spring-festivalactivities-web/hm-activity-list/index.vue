55686398b23ec08893030e5ea71bfb064f8dcf58
胡小根 <hxg@haomo.com>
2024-02-07 11:24:09 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/components/decathlon-spring-festivalactivities-web/hm-activity-list/index.vue
+++ src/components/decathlon-spring-festivalactivities-web/hm-activity-list/index.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="activityList flex-col">
-    <div
-      v-for="(item, index) in cList"
-      :key="index"
-      class="activityItem flex-col"
-    >
+    <div v-for="(item, index) in cList" :key="index" class="activityItem flex-col">
+      <span class="item-label">渠道:{{item.label}}</span>
+      
       <div class="item-url flex-row">
         <span class="item-label">URL:</span>
-        <a-input v-model:value="item.url" placeholder="活动链接" disabled/>
-        <a-button type="primary" @click="copyLink(item, item.url)"
-          >复制链接</a-button
-        >
+        <a-input v-model:value="item.url" placeholder="活动链接" disabled />
+        <a-button type="primary" @click="copyLink(item, item.url)">复制链接</a-button>
       </div>
       <div class="item-qrcode flex-row">
         <span class="item-label">二维码:</span>
@@ -45,6 +41,7 @@ export default {
     list: {
       handler: function (value, oldValue) {
         this.cList = value;
+        this.setQrcode()
       },
       deep: true,
     },
@@ -61,6 +58,20 @@ export default {
     copyLink(e, str) {
       this.$emit("copyLink", e, str);
     },
+    setQrcode() {
+      setTimeout(() => {
+        this.cList.forEach((e) => {
+          const container = document.querySelector(`#qrcode${e.id}`);
+          new this.$QrCode(container, {
+            text: e.url,
+            width: 128,
+            height: 128,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+          });
+        })
+      });
+    }
   },
 };
 </script>
@@ -69,36 +80,43 @@ export default {
   display: flex;
   flex-direction: column;
 }
+
 .flex-row {
   display: flex;
   flex-direction: row;
 }
+
 .activityList {
   width: 100%;
   padding: 0px 12px;
   padding-right: 24px;
   row-gap: 24px;
   box-sizing: border-box;
+
   .activityItem {
     width: 100%;
     row-gap: 12px;
+
     .item-url {
       width: 100%;
       align-items: center;
       column-gap: 8px;
     }
+
     .item-label {
       min-width: 50px;
       font-size: 14px;
       color: #333;
       text-align: left;
     }
+
     .item-qrcode {
       width: 100%;
       column-gap: 8px;
+
       &-box {
-        min-width: 200px;
-        min-height: 200px;
+      //   min-width: 200px;
+      //   min-height: 200px;
         background: #cccccc26;
       }
     }
