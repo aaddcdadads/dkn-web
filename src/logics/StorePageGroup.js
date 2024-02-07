7ad0a3987736f28a6ae1f6228dd3f1e4af8f45e4
胡小根 <hxg@haomo.com>
2024-02-07 20:10:36 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/logics/StorePageGroup.js
+++ src/logics/StorePageGroup.js
@@ -322,7 +322,7 @@ const addStore = (logic.addStore = async (pageVm, eventData) => {
   };
   self.$getAction(urls, paramsEs).then((resTypes) => {
     console.log("编辑base64", resTypes);
-    if (resTypes.records.length != 0) {
+    if (resTypes.result.records.length != 0) {
       self.$message.error("店名重复");
 
       return;
