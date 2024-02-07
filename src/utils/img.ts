4122c901d141e7b54fd4e54f795c3fc64870dd66
daijianhui <daijianhui@haomo-studio.com>
2024-02-07 09:57:58 +0800

    refactor: 增加压缩包名称参数

--- src/utils/img.ts
+++ src/utils/img.ts
@@ -9,14 +9,15 @@ interface Img {
 /**
  * 多个图片压缩成zip导出
  * @param arr base64图片
+ * @param zipName 压缩包名称
  */
-export default function downloadImgZip(arr: Array<Img>) {
+export default function downloadImgZip(arr: Array<Img>, zipName = '图片.zip') {
   const zip = new JSZip();
   arr.forEach(({ fileName, base64 }) => {
     zip.file(fileName + '.png', splitBase64(base64), { base64: true });
   });
   zip.generateAsync({ type: 'blob' }).then((content) => {
-    FileSaver.saveAs(content, '二维码.zip');
+    FileSaver.saveAs(content, zipName);
   });
 }
 
