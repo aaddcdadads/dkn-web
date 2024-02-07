058bec987424b778d70dc883b3176e58732a46b8
胡小根 <hxg@haomo.com>
2024-02-07 15:54:41 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/logics/OrderListGroup.js
+++ src/logics/OrderListGroup.js
@@ -12,44 +12,46 @@ let logic = {};
 /**
  * 逻辑流 orderRefund 入口函数
  */
-const orderRefund = logic.orderRefund = async (pageVm, eventData) => {
-  console.log(`orderRefund: `, pageVm, eventData)
+const orderRefund = (logic.orderRefund = async (pageVm, eventData) => {
+  console.log(`orderRefund: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.orderRefundData = eventData;
 
-  
   self.viewRegistrationOrdersDeleteModal.visible = false;
-let order = self.orderRefundData[0]
-if(!order || !order.id){
-    order = self.registrationOrder
-}
-if(!order.paymentChannel || (order.paymentChannel != "0" && order.paymentChannel != "1")){
-    return self.$message.error("支付渠道错误")
-}
-if(order.paymentChannel == "0"){
+  let order = self.orderRefundData[0];
+  if (!order || !order.id) {
+    order = self.registrationOrder;
+  }
+  if (
+    !order.paymentChannel ||
+    (order.paymentChannel != "0" && order.paymentChannel != "1")
+  ) {
+    return self.$message.error("支付渠道错误");
+  }
+  if (order.paymentChannel == "0") {
     //wx
-    self.$getAction("/api/wechat/refund", {orderId: order.id}).then(res => {
-        if(res && res.indexOf("SUCCESS") > -1){
-            return self.$message.success("【微信】申请退款成功")
-        }else{
-            return self.$message.error("【微信】申请退款失败")
-        }
-    })
-}
-if(order.paymentChannel == "1"){
+    self.$getAction("/api/wechat/refund", { orderId: order.id }).then((res) => {
+      if (res && res.indexOf("SUCCESS") > -1) {
+        return self.$message.success("【微信】申请退款成功");
+      } else {
+        return self.$message.error("【微信】申请退款失败");
+      }
+    });
+  }
+  if (order.paymentChannel == "1") {
     //zfb
-    self.$getAction("/api/aliPay/tradeRefund", {orderId: order.id}).then(res => {
-        if(res.success){
-            self.$refs.viewRegistrationOrdersTable.getData()
-            return self.$message.success("【支付宝】退款成功")
-        }else{
-            return self.$message.error("【支付宝】退款失败")
+    self
+      .$getAction("/api/aliPay/tradeRefund", { orderId: order.id })
+      .then((res) => {
+        if (res.success) {
+          self.$refs.viewRegistrationOrdersTable.getData();
+          return self.$message.success("【支付宝】退款成功");
+        } else {
+          return self.$message.error("【支付宝】退款失败");
         }
-    })
-}
-
-
-}
+      });
+  }
+});
 
 /********************** end orderRefund 开始 *********************/
 
@@ -58,20 +60,21 @@ if(order.paymentChannel == "1"){
 /**
  * 逻辑流 ordersBatchRefund 入口函数
  */
-const ordersBatchRefund = logic.ordersBatchRefund = async (pageVm, eventData) => {
-  console.log(`ordersBatchRefund: `, pageVm, eventData)
+const ordersBatchRefund = (logic.ordersBatchRefund = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`ordersBatchRefund: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.ordersBatchRefundData = eventData;
 
-  
-  console.log("this.onSelectList", self.onSelectList)
-self.onSelectList.forEach(item => {
-    self.orderRefund(self, [item])
-})
-self.batchRefundModal.visible = false;
-location.reload();
-
-}
+  console.log("this.onSelectList", self.onSelectList);
+  self.onSelectList.forEach((item) => {
+    self.orderRefund(self, [item]);
+  });
+  self.batchRefundModal.visible = false;
+  location.reload();
+});
 
 /********************** end ordersBatchRefund 开始 *********************/
 
@@ -79,41 +82,36 @@ location.reload();
 /**
  * 发送修改请求
  */
-const editRequest = logic.editRequest = async function () {
-  let res = await self.$putAction(
-    `/api/restify/viewRegistrationOrders/edit`,
-    {
+const editRequest = (logic.editRequest = async function () {
+  let res = await self.$putAction(`/api/restify/viewRegistrationOrders/edit`, {
     ...self.$refs.viewRegistrationOrdersEditForm.getFormValues(),
-    id: self.currentViewRegistrationOrdersId
-}
-  )
+    id: self.currentViewRegistrationOrdersId,
+  });
   self.editRequestData = res;
-}
-
+});
 
 /**
  * 逻辑流 editViewRegistrationOrders 入口函数
  */
-const editViewRegistrationOrders = logic.editViewRegistrationOrders = async (pageVm, eventData) => {
-  console.log(`editViewRegistrationOrders: `, pageVm, eventData)
+const editViewRegistrationOrders = (logic.editViewRegistrationOrders = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`editViewRegistrationOrders: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.editViewRegistrationOrdersData = eventData;
 
-  
-  await self.$refs.viewRegistrationOrdersEditForm.validate()
+  await self.$refs.viewRegistrationOrdersEditForm.validate();
   await editRequest();
-  if(self.editRequestData.success){
+  if (self.editRequestData.success) {
     self.$message.success("编辑成功");
-  self.$refs.viewRegistrationOrdersEditForm.reset();
-self.viewRegistrationOrdersEditModal.visible = false;
-  self.$refs.viewRegistrationOrdersTable.getData()
-
-}else{
+    self.$refs.viewRegistrationOrdersEditForm.reset();
+    self.viewRegistrationOrdersEditModal.visible = false;
+    self.$refs.viewRegistrationOrdersTable.getData();
+  } else {
     self.$message.error("编辑失败");
-
-}
-
-}
+  }
+});
 
 /********************** end editViewRegistrationOrders 开始 *********************/
 
@@ -121,40 +119,35 @@ self.viewRegistrationOrdersEditModal.visible = false;
 /**
  * 发送添加请求
  */
-const addRequest = logic.addRequest = async function () {
-  let res = await self.$postAction(
-    `/api/restify/viewRegistrationOrders/add`,
-    {
-    ...self.$refs.viewRegistrationOrdersAddForm.getFormValues()
-}
-  )
+const addRequest = (logic.addRequest = async function () {
+  let res = await self.$postAction(`/api/restify/viewRegistrationOrders/add`, {
+    ...self.$refs.viewRegistrationOrdersAddForm.getFormValues(),
+  });
   self.addRequestData = res;
-}
-
+});
 
 /**
  * 逻辑流 addViewRegistrationOrders 入口函数
  */
-const addViewRegistrationOrders = logic.addViewRegistrationOrders = async (pageVm, eventData) => {
-  console.log(`addViewRegistrationOrders: `, pageVm, eventData)
+const addViewRegistrationOrders = (logic.addViewRegistrationOrders = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`addViewRegistrationOrders: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.addViewRegistrationOrdersData = eventData;
 
-  
-  await self.$refs.viewRegistrationOrdersAddForm.validate()
+  await self.$refs.viewRegistrationOrdersAddForm.validate();
   await addRequest();
-  if(self.addRequestData.success){
+  if (self.addRequestData.success) {
     self.$message.success("添加成功");
-  self.$refs.viewRegistrationOrdersAddForm.reset();
-self.viewRegistrationOrdersAddModal.visible = false;
-  self.$refs.viewRegistrationOrdersTable.getData()
-
-}else{
+    self.$refs.viewRegistrationOrdersAddForm.reset();
+    self.viewRegistrationOrdersAddModal.visible = false;
+    self.$refs.viewRegistrationOrdersTable.getData();
+  } else {
     self.$message.error("添加失败");
-
-}
-
-}
+  }
+});
 
 /********************** end addViewRegistrationOrders 开始 *********************/
 
@@ -163,20 +156,24 @@ self.viewRegistrationOrdersAddModal.visible = false;
 /**
  * 逻辑流 exportViewRegistrationOrders 入口函数
  */
-const exportViewRegistrationOrders = logic.exportViewRegistrationOrders = async (pageVm, eventData) => {
-  console.log(`exportViewRegistrationOrders: `, pageVm, eventData)
+const exportViewRegistrationOrders = (logic.exportViewRegistrationOrders = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`exportViewRegistrationOrders: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.exportViewRegistrationOrdersData = eventData;
 
-  
   let url = "/api/restify/viewRegistrationOrders/exportExcel";
-let params = {
+  let params = {
     ...self.viewRegistrationOrdersTable.params,
-    ...self.$getFilterValues(self.$refs.viewRegistrationOrdersFilter.getFormValues(), self.$refs.viewRegistrationOrdersFilter.cSchema),
-};
-self.$downloadFile("view_registration_orders列表数据.xlsx", url, params);
-
-}
+    ...self.$getFilterValues(
+      self.$refs.viewRegistrationOrdersFilter.getFormValues(),
+      self.$refs.viewRegistrationOrdersFilter.cSchema
+    ),
+  };
+  self.$downloadFile("view_registration_orders列表数据.xlsx", url, params);
+});
 
 /********************** end exportViewRegistrationOrders 开始 *********************/
 
@@ -184,39 +181,37 @@ self.$downloadFile("view_registration_orders列表数据.xlsx", url, params);
 /**
  * 发送删除请求
  */
-const deleteRequest = logic.deleteRequest = async function () {
+const deleteRequest = (logic.deleteRequest = async function () {
   let res = await self.$deleteAction(
     `/api/restify/viewRegistrationOrders/delete`,
     {
-    id: self.currentViewRegistrationOrdersId,
-    "databaseId": ""
-}
-  )
+      id: self.currentViewRegistrationOrdersId,
+      databaseId: "",
+    }
+  );
   self.deleteRequestData = res;
-}
-
+});
 
 /**
  * 逻辑流 deleteViewRegistrationOrders 入口函数
  */
-const deleteViewRegistrationOrders = logic.deleteViewRegistrationOrders = async (pageVm, eventData) => {
-  console.log(`deleteViewRegistrationOrders: `, pageVm, eventData)
+const deleteViewRegistrationOrders = (logic.deleteViewRegistrationOrders = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`deleteViewRegistrationOrders: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.deleteViewRegistrationOrdersData = eventData;
 
-  
   await deleteRequest();
-  if(self.deleteRequestData.success){
+  if (self.deleteRequestData.success) {
     self.$message.success("删除成功");
-  self.$refs.viewRegistrationOrdersTable.getData()
-self.viewRegistrationOrdersDeleteModal.visible = false
-
-}else{
+    self.$refs.viewRegistrationOrdersTable.getData();
+    self.viewRegistrationOrdersDeleteModal.visible = false;
+  } else {
     self.$message.error("删除失败");
-
-}
-
-}
+  }
+});
 
 /********************** end deleteViewRegistrationOrders 开始 *********************/
 
@@ -225,15 +220,20 @@ self.viewRegistrationOrdersDeleteModal.visible = false
 /**
  * 逻辑流 downloadTemplate 入口函数
  */
-const downloadTemplate = logic.downloadTemplate = async (pageVm, eventData) => {
-  console.log(`downloadTemplate: `, pageVm, eventData)
+const downloadTemplate = (logic.downloadTemplate = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`downloadTemplate: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.downloadTemplateData = eventData;
 
-  
-  self.$downloadFile("view_registration_orders列表数据模板.xlsx", "/api/restify/viewRegistrationOrders/downExcelTemplate", {})
-
-}
+  self.$downloadFile(
+    "view_registration_orders列表数据模板.xlsx",
+    "/api/restify/viewRegistrationOrders/downExcelTemplate",
+    {}
+  );
+});
 
 /********************** end downloadTemplate 开始下载模板 *********************/
 
@@ -242,27 +242,30 @@ const downloadTemplate = logic.downloadTemplate = async (pageVm, eventData) => {
 /**
  * 逻辑流 searchViewRegistrationOrders 入口函数
  */
-const searchViewRegistrationOrders = logic.searchViewRegistrationOrders = async (pageVm, eventData) => {
-  console.log(`searchViewRegistrationOrders: `, pageVm, eventData)
+const searchViewRegistrationOrders = (logic.searchViewRegistrationOrders = async (
+  pageVm,
+  eventData
+) => {
+  console.log(`searchViewRegistrationOrders: `, pageVm, eventData);
   self = Object.assign(pageVm, logic);
   self.searchViewRegistrationOrdersData = eventData;
 
-  
   let params = {
     ...self.viewRegistrationOrdersTable.params,
-    ...self.$getFilterValues(self.$refs.viewRegistrationOrdersFilter.getFormValues(), self.$refs.viewRegistrationOrdersFilter.cSchema),
-}
+    ...self.$getFilterValues(
+      self.$refs.viewRegistrationOrdersFilter.getFormValues(),
+      self.$refs.viewRegistrationOrdersFilter.cSchema
+    ),
+  };
 
-if (_.isEqual(params, self.viewRegistrationOrdersTable.params)) {
-    self.$refs.viewRegistrationOrdersTable.getData()
-} else {
-    self.viewRegistrationOrdersTable.params = params
-}
-
-}
+  if (_.isEqual(params, self.viewRegistrationOrdersTable.params)) {
+    self.$refs.viewRegistrationOrdersTable.getData();
+  } else {
+    self.viewRegistrationOrdersTable.params = params;
+  }
+});
 
 /********************** end searchViewRegistrationOrders 开始 *********************/
-
 
 export {
   orderRefund,
@@ -276,4 +279,4 @@ export {
   deleteViewRegistrationOrders,
   downloadTemplate,
   searchViewRegistrationOrders,
-}
+};
