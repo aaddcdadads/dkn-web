b4d188f961fd1ac03cc7705df43f779cdb960dde
胡小根 <hxg@haomo.com>
2024-02-08 15:03:03 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/logics/StorePageGroup.js
+++ src/logics/StorePageGroup.js
@@ -361,7 +361,7 @@ const addStore = (logic.addStore = async (pageVm, eventData) => {
     }
   });
 
-  if (self.num == 0) {
+  if (self.num == 1) {
     await self.$refs.storeAddForm.validate();
     var values = self.$refs.storeAddForm.getFormValues();
 
