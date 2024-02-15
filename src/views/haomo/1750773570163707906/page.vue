5c0a3e6ff09c3da7509004f7fde382c3ab9824e0
胡小根 <hxg@haomo.com>
2024-02-15 09:56:04 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/views/haomo/1750773570163707906/page.vue
+++ src/views/haomo/1750773570163707906/page.vue
@@ -676,8 +676,8 @@ export default {
                   "x-component-props": {
                     title: "",
                     onChange: function (e) {
-                      this.editNum = 1;
-                      console.log("e=====", e, this.editNum);
+                      self.editNum = 1;
+                      console.log("e=====", e, self.editNum);
                     },
                   },
                   "x-component": "HmAntInput",
