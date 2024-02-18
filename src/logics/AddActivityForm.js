d5ab49a8893c5c2595f014b9c65ba202b4fc44a9
胡小根 <hxg@haomo.com>
2024-02-18 10:24:51 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

--- src/logics/AddActivityForm.js
+++ src/logics/AddActivityForm.js
@@ -11,133 +11,136 @@ let logic = {};
 /**
  * 发送添加请求
  */
-const editRequest = (logic.editRequest = async function () {
-  let res = await self.$postAction(`/api/dkn/activity/adds`, self.item);
+const editRequest = logic.editRequest = async function () {
+  let res = await self.$postAction(
+    `/api/dkn/activity/adds`,
+    self.item
+  )
   self.editRequestData = res;
-});
+}
+
 
 /**
  * 逻辑流 addActivty 入口函数
  */
-const addActivty = (logic.addActivty = async (pageVm, eventData) => {
-  console.log(`addActivty: `, pageVm, eventData);
+const addActivty = logic.addActivty = async (pageVm, eventData) => {
+  console.log(`addActivty: `, pageVm, eventData)
   self = Object.assign(pageVm, logic);
   self.addActivtyData = eventData;
 
-  await self.$refs.activityForm.validate();
-  await self.$refs.activityExtTwoForm.validate();
+  
+  await self.$refs.activityForm.validate()
+await self.$refs.activityExtTwoForm.validate()
 
-  let activityProjects = [],
-    activityImgs = [],
-    activityExts = [];
-  activityExts = [
+let activityProjects=[],activityImgs=[],activityExts=[]
+activityExts=[
     {
-      activityRules: self.activityRules.value,
-      protocol: self.agreementCommitmentletter.value,
-      customerService: self.customerService.value,
-      ...self.$refs.activityExtTwoForm.getFormValues(),
-    },
-  ];
-  if (self.$refs.activityProjectTable.cData.length > 0) {
-    activityProjects = self.$refs.activityProjectTable.cData.map((x) => {
-      return x;
-    });
-  }
+        activityRules:self.activityRules.value,
+        protocol:self.agreementCommitmentletter.value,
+        customerService:self.customerService.value,
+    ...self.$refs.activityExtTwoForm.getFormValues()}]
+if(self.$refs.activityProjectTable.cData.length > 0){
+    activityProjects = self.$refs.activityProjectTable.cData.map((x)=>{
+        return x
+    })
+}
 
-  if (self.$refs.activityImgTableOne.cData.length > 0) {
-    activityImgs = self.$refs.activityImgTableOne.cData.map((x) => {
-      x.type = 0;
-      return x;
-    });
-  }
-  if (self.$refs.activityImgTableTwo.cData.length > 0) {
-    let list = self.$refs.activityImgTableTwo.cData.map((x) => {
-      x.type = 1;
-      return x;
-    });
-    activityImgs = [...activityImgs, ...list];
-  }
-  if (self.$refs.sharingImageTable.cData.length > 0) {
-    let list = self.$refs.sharingImageTable.cData.map((x) => {
-      x.type = 2;
-      return x;
-    });
-    activityImgs = [...activityImgs, ...list];
-  }
-  let item = self.$refs.activityForm.getFormValues();
-  item.startTime =
-    self.$moment(item.cycle[0]).format("YYYY-MM-DD") + " 00:00:00";
-  item.endTime = self.$moment(item.cycle[1]).format("YYYY-MM-DD") + " 23:59:59";
-  delete item.cycle;
+if(self.$refs.activityImgTableOne.cData.length > 0){
+    activityImgs = self.$refs.activityImgTableOne.cData.map((x)=>{
+        x.type=0
+        return x
+    })
+}
+if(self.$refs.activityImgTableTwo.cData.length > 0){
+     let list = self.$refs.activityImgTableTwo.cData.map((x)=>{
+        x.type=1
+        return x
+    })
+    activityImgs =[...activityImgs,...list]
+}
+if(self.$refs.sharingImageTable.cData.length > 0){
+     let list = self.$refs.sharingImageTable.cData.map((x)=>{
+        x.type=2
+        return x
+    })
+    activityImgs =[...activityImgs,...list]
+}
+let item =self.$refs.activityForm.getFormValues()
+    item.startTime = self.$moment(item.cycle[0]).format('YYYY-MM-DD')+" 00:00:00"
+    item.endTime = self.$moment(item.cycle[1]).format('YYYY-MM-DD')+" 23:59:59"
+    delete item.cycle
 
-  item.pickUpStartTime =
-    self.$moment(item.pickUpTime[0]).format("YYYY-MM-DD") + " 00:00:00";
-  item.pickUpEndTime =
-    self.$moment(item.pickUpTime[1]).format("YYYY-MM-DD") + " 23:59:59";
-  delete item.pickUpTime;
+    item.pickUpStartTime = self.$moment(item.pickUpTime[0]).format('YYYY-MM-DD')+" 00:00:00"
+    item.pickUpEndTime = self.$moment(item.pickUpTime[1]).format('YYYY-MM-DD')+" 23:59:59"
+    delete item.pickUpTime
 
-  let expense = 0;
-  activityProjects.forEach((e) => {
-    expense += e.expense;
-  });
-  if (activityProjects.length === 0) {
-    self.$message.error("请添加至少一个活动项目");
-    return;
-  }
-  if (!self.bgColour) {
-    self.$message.error("请设置报名页背景颜色");
-    return;
-  }
-  if (!self.colour) {
-    self.$message.error("请设置报名页信息框颜色");
-    return;
-  }
-  if (!self.textColour) {
-    self.$message.error("请设置报名页信息框字体颜色");
-    return;
-  }
-  let closeTime = new Date(item.closeTime).getTime();
-  let startTime = new Date(item.startTime).getTime();
-  let endTime = new Date(item.endTime).getTime();
-  let pickUpTime = new Date(item.pickUpTime).getTime();
-  if (closeTime < startTime) {
+let expense=0
+    activityProjects.forEach(e=>{
+        expense+=e.expense
+    })
+if(activityProjects.length === 0){
+    self.$message.error("请添加至少一个活动项目")
+    return
+}
+if(!self.bgColour){
+    self.$message.error("请设置报名页背景颜色")
+    return
+}
+if(!self.colour){
+    self.$message.error("请设置报名页信息框颜色")
+    return
+}
+if(!self.textColour){
+    self.$message.error("请设置报名页信息框字体颜色")
+    return
+}
+let closeTime = new Date(item.closeTime).getTime()
+let startTime = new Date(item.startTime).getTime()
+let endTime = new Date(item.endTime).getTime()
+let pickUpTime = new Date(item.pickUpTime).getTime()
+if(closeTime <startTime){
     //self.$message.error("报名截止时间不能小于活动周期的开始时间")
     //return
-  }
-  if (closeTime > endTime) {
-    // self.$message.error("报名截止时间不能大于活动周期的结束时间")
-    // return
-  }
-  if (pickUpTime < closeTime) {
+}
+if(closeTime >endTime){
+    self.$message.error("报名截止时间不能大于活动周期的结束时间")
+    return
+}
+if(pickUpTime <closeTime){
     // self.$message.error("核销截止时间不能小于报名截止的时间")
     // return
-  }
-  // if(pickUpTime >startTime){
-  //     self.$message.error("核销截止时间不能小于活动周期的开始时间")
-  //     return
-  // }
-  self.item = {
+}
+// if(pickUpTime >startTime){
+//     self.$message.error("核销截止时间不能小于活动周期的开始时间")
+//     return
+// }
+self.item={
     ...item,
     activityProjects,
     activityImgs,
     activityExts,
     expense,
-    bgColour: self.bgColour,
-    colour: self.colour,
-    textColour: self.textColour,
-  };
+    bgColour:self.bgColour,
+    colour:self.colour,
+    textColour:self.textColour
+}
 
   await editRequest();
-  if (!self.editRequestData.success) {
-    self.$message.error(self.editRequestData.message);
-    return;
-  }
-  self.$message.success("操作成功");
-  self.$router.push({
+  if(!self.editRequestData.success){
+    self.$message.error(self.editRequestData.message)
+    return
+}
+self.$message.success("操作成功")
+self.$router.push({
     path: `/haomo/1750448384092672002/page`,
   });
-});
+
+}
 
 /********************** end addActivty 开始 *********************/
 
-export { editRequest, addActivty };
+
+export {
+  editRequest,
+  addActivty,
+}
