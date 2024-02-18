66d1c83c3b9772b31aa77c8e7d6a929da52147ff
daijianhui <daijianhui@haomo-studio.com>
2024-02-18 20:33:40 +0800

    refactor: 调整dkn正式环境的部署脚本

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,5 +3,5 @@ set -x
 rm -rf src/views/haomo/test
 # cp src/views/haomo/1750448384092672002/page.vue src/views/dashboard/Analysis/index.vue
 cp docs/compontents/HmColorKit.vue src/components/built-in/jeecg/HmColorKit.vue
-npm run build
+npm run build:dkn
 rsync -avz --delete --progress dist/* sports@121.4.21.205:/var/www/html/dkn
