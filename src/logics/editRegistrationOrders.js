4fc47b845362e71f60a7089edd572777a592be45
胡小根 <hxg@haomo.com>
2024-04-03 17:56:59 +0800

    feat: 迪卡侬春节活动-web端-更新设计云页面清单

---
+++ src/logics/editRegistrationOrders.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+import moment from "moment";
+
+// self对象保存页面的this对象
+let self = {};
+
+// logic对象保存当前逻辑组对象，可以通过logic.func或者self.func来调用本逻辑组内的所有逻辑单元
+let logic = {};
+
+export {};
